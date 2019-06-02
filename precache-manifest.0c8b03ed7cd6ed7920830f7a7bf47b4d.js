50f36368c89da1cbcdce8c2668b2530d8296a000
codereber <jamesmpyle@gmail.com>
2019-06-02 12:45:36 -0400

    Updates

---
+++ precache-manifest.0c8b03ed7cd6ed7920830f7a7bf47b4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e3008f1e006f9047fc92f7ddf346cff",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "df4c325b897ddc4f9fda",
+    "url": "/clickygame/static/css/main.6119bade.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clickygame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "df4c325b897ddc4f9fda",
+    "url": "/clickygame/static/js/main.cfb91696.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  }
+]);
